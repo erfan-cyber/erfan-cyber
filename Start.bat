715d825099fa5675c2ac0ae615977df88d2478b5
erfan-cyber <74706544+erfan-cyber@users.noreply.github.com>
2021-02-16 01:48:01 +0330

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -2,51 +2,100 @@
 Color e
 Echo Welcome To Erfan-Cyber Server Runner 
 Pause
+Cls
 Echo Please Change The minecraft_1.?.?.jar To == Server !
 Pause
+Cls
 Echo For Start Making And Running Server Please : 
 Pause
+Cls
 Title Making Server
+Cls
 Echo making server .
+Cls
 Echo making server ..
+Cls
 Echo making server ...
+Cls
 Echo making server .
+Cls
 Echo making server ..
+Cls
 Echo making server ...
+Cls
 Echo making server .
+Cls
 Echo making server ..
+Cls
 Echo making server ...
+Cls
 Echo making server .
+Cls
 Echo making server ..
+Cls
 Echo making server ...
+Cls
 Echo making server .
+Cls
 Echo making server ..
+Cls
 Echo making server ...
+Cls
 Echo making server .
+Cls
 Echo making server ..
+Cls
 Echo making server ...
+Cls
 Echo making server .
+Cls
 Echo making server ..
+Cls
 Echo making server ...
+Cls
 Title Done !
+Cls
 Echo .
+Cls
 Echo .
+Cls
 Echo .
+Cls
 Echo .
+Cls
 Echo .
+Cls
 Title Server Running
+Cls
 Echo starting server .
+Cls
 Echo starting server ..
+Cls
 Echo starting server ...
+Cls
 Echo starting server .
+Cls
 Echo starting server ..
+Cls
 Echo starting server ...
+Cls
 Echo starting server .
+Cls
 Echo starting server ..
+Cls
 Echo starting server ...
+Cls
 Echo starting server .
+Cls
 Echo starting server ..
+Cls
 Echo starting server ...
+Cls
 java -Xmx1024M -Xms1024M -jar Server.jar nogui
+Cls
 Title stopped
+Cls
+Echo [!] The Server Has Stopped.
+Echo [*] For Exit The Console Please : 
 Pause
+Cls
