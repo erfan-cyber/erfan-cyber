348cdde0bfc0a86acc8486e2f680d4e18bafb259
erfan-cyber <74706544+erfan-cyber@users.noreply.github.com>
2021-02-16 01:39:40 +0330

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,4 +1,52 @@
 @echo off
 Color e
-Echo hello world
+Echo Welcome To Erfan-Cyber Server Runner 
+Pause
+Echo Please Change The minecraft_1.?.?.jar To == Server !
+Pause
+Echo For Start Making And Running Server Please : 
+Pause
+Title Making Server
+Echo making server .
+Echo making server ..
+Echo making server ...
+Echo making server .
+Echo making server ..
+Echo making server ...
+Echo making server .
+Echo making server ..
+Echo making server ...
+Echo making server .
+Echo making server ..
+Echo making server ...
+Echo making server .
+Echo making server ..
+Echo making server ...
+Echo making server .
+Echo making server ..
+Echo making server ...
+Echo making server .
+Echo making server ..
+Echo making server ...
+Title Done !
+Echo .
+Echo .
+Echo .
+Echo .
+Echo .
+Title Server Running
+Echo starting server .
+Echo starting server ..
+Echo starting server ...
+Echo starting server .
+Echo starting server ..
+Echo starting server ...
+Echo starting server .
+Echo starting server ..
+Echo starting server ...
+Echo starting server .
+Echo starting server ..
+Echo starting server ...
+java -Xmx1024M -Xms1024M -jar Server.jar nogui
+Title stopped
 Pause
